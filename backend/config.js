7fedcb4be6fdf60f9bd69f6df6a355cc0d9f333c
Mustafa Kapasi <102843816+Mustu19@users.noreply.github.com>
2024-03-10 15:19:56 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555 ;
 
-export const mongoDBURL = "mongodb+srv://codewithmustu:codewithmustu@bookstore.eh1amtb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore"
+export const mongoDBURL = "Your MongoDB Atlas URL"
