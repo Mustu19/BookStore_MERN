46bf564ab71bec816fd6412b0aa7c9e790f7aa70
Mustu19 <codewithmustu@gmail.com>
2024-03-11 18:18:01 +0530

    Create Book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
-// import CreateBook from './pages/CreateBooks' ;
+import CreateBook from './pages/CreateBooks' ;
 // import EditBook from './pages/EditBook' ;
 // import DeleteBook from './pages/DeleteBook' ;
 import ShowBook from './pages/ShowBook';
@@ -13,8 +13,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/books/details/:id' element={<ShowBook />} />
-        {/* <Route path='/books/create' element={<CreateBook />} />
-        <Route path='/books/edit/:id' element={<EditBook />} />
+        <Route path='/books/create' element={<CreateBook />} />
+        {/* <Route path='/books/edit/:id' element={<EditBook />} />
         <Route path='/books/delete/:id' element={<DeleteBook />} /> */}
       </Routes>
     </>
